c334032ea3df2a5c07fabd8101dfdfc92f97dba0
armiiiiiin <forutanmohammad@gmail.com>
2025-02-19 13:29:43 +0330

    [ADD] static data and type

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import "./App.module.css";
 import Header from "./components/Header/Header";
 import Toolbar from "./components/Toolbar/Toolbar";
 import Result from "./components/Result/Result";
+import { Dream } from "./type/dream";
+
+const dreams: Dream[] = [
+  {
+    id: "1",
+    title: "school",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    data: new Date(2025, 1, 14),
+    vibe: "good",
+  },
+];
 
 function App() {
   return (
@@ -9,7 +20,11 @@ function App() {
       <Header />
       <main>
         <Toolbar />
-        <Result />
+        <div className="result">
+          {dreams.map((dream) => (
+            <li>{dream.title}</li>
+          ))}
+        </div>
       </main>
     </>
   );
---
+++ src/type/dream.ts
@@ -0,0 +1,9 @@
+import { Vibe } from "./vibe";
+
+export type Dream = {
+  id: string;
+  title: string;
+  vibe: Vibe;
+  description: string;
+  data: Date;
+};
