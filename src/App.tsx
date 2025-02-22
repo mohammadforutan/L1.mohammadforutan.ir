e421a71d4364793329732b19593f0421aa377669
armiiiiiin <forutanmohammad@gmail.com>
2025-02-22 11:47:21 +0330

    [FIX] result component

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,6 @@ import "./App.module.css";
 import Header from "./components/Header/Header";
 import Toolbar from "./components/Toolbar/Toolbar";
 import Result from "./components/Result/Result";
-import { Dream } from "./type/dream";
-
-const dreams: Dream[] = [
-  {
-    id: "1",
-    title: "school",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    data: new Date(2025, 1, 14),
-    vibe: "good",
-  },
-];
 
 function App() {
   return (
@@ -20,11 +9,7 @@ function App() {
       <Header />
       <main>
         <Toolbar />
-        <div className="result">
-          {dreams.map((dream) => (
-            <li>{dream.title}</li>
-          ))}
-        </div>
+        <Result />
       </main>
     </>
   );
--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,13 +1,31 @@
 import { ReactNode } from "react";
 import styles from "./Result.module.css";
+import { Dream } from "../../type/dream";
+
+const dreams: Dream[] = [
+  {
+    id: "1",
+    title: "school",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    data: new Date(2025, 1, 14),
+    vibe: "good",
+  },
+  {
+    id: "2",
+    title: "park",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    data: new Date(2025, 1, 14),
+    vibe: "bad",
+  },
+];
 
 export default function Result(): ReactNode {
   return (
     <div className={styles.result}>
       <ul className="result">
-        <li>NOTE #1</li>
-        <li>NOTE #2</li>
-        <li>NOTE #3</li>
+        {dreams.map((dream) => (
+          <li key={dream.id}>{dream.title}</li>
+        ))}
       </ul>
     </div>
   );
