8014aa71b70d4c188638a88e73a0eecbbe56a5a8
armiiiiiin <forutanmohammad@gmail.com>
2025-02-22 13:25:58 +0330

    [ADD] color prop to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,21 +2,23 @@ import { ComponentProps, ReactNode } from "react";
 import styles from "./Button.module.css";
 import { clsx } from "clsx";
 
-type Variant = "solid" | "outline";
-type Size = "medium" | "large";
+type Variant = "solid" | "outline" | "ghost";
+type Size = "small" | "medium" | "large";
 type Shape = "rectangle" | "square" | "circle";
+type Color = "primary" | "danger";
 
 type Props = ComponentProps<"button"> & {
   variant?: Variant;
   size?: Size;
   shape?: Shape;
+  color?: Color;
 };
 
 export default function Button({
   variant = "solid",
   size = "medium",
   shape = "rectangle",
-
+  color = "primary",
   children,
   className,
   ...otherProps
@@ -27,6 +29,7 @@ export default function Button({
         styles.button,
         styles[variant],
         styles[size],
+        styles[color],
         styles[shape],
         className,
       )}
